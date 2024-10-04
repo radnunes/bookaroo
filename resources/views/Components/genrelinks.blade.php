3a709e12401d3615748a7dc36d80312dfd726a19
sleeper <sleepervoid.c@gmail.com>
2024-10-04 13:10:00 +0100

    books->genres UI fix

--- resources/views/Components/genrelinks.blade.php
+++ resources/views/Components/genrelinks.blade.php
@@ -76,33 +76,22 @@
                 <table class="table table-striped table-hover">
                     <thead>
                     <tr>
-                        <th>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="chall">
-                                <label class="custom-control-label" for="d1"></label>
-                            </div>
-                        </th>
                         <th>ID</th>
-                        <th>Name</th>
-                        <th>Address</th>
-                        <th>Phone</th>
-                        <th>Date</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Nº of Pages</th>
+                        <th>Publication Date</th>
                         <th>Action</th>
                     </tr>
                     </thead>
                     <tbody>
+                    <!-- foreach -->
                     <tr>
-                        <td>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="d1">
-                                <label class="custom-control-label" for="d1"></label>
-                            </div>
-                        </td>
-                        <td>2474</td>
-                        <td>Brown, Asher D.</td>
-                        <td>Ap #331-7123 Lobortis Avenue</td>
-                        <td>(958) 421-0798</td>
-                        <td>13/09/2020</td>
+                        <td>22</td>
+                        <td>Harry Potter and the Seven Dwarves</td>
+                        <td>J. K. Rowling</td>
+                        <td>123</td>
+                        <td>13/09/2025</td>
                         <td>
                             <div class="dropdown">
                                 <button class="btn btn-sm dropdown-toggle" type="button" id="dr1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -111,115 +100,11 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dr1" style="">
                                     <a class="dropdown-item" href="#">Edit</a>
                                     <a class="dropdown-item" href="#">Remove</a>
-                                    <a class="dropdown-item" href="#">Assign</a>
                                 </div>
                             </div>
                         </td>
                     </tr>
-                    <!-- @foreach -->
-                    <tr>
-                        <td>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="d2">
-                                <label class="custom-control-label" for="d2"></label>
-                            </div>
-                        </td>
-                        <td>2786</td>
-                        <td>Leblanc, Yoshio V.</td>
-                        <td>287-8300 Nisl. St.</td>
-                        <td>(899) 881-3833</td>
-                        <td>04/05/2019</td>
-                        <td>
-                            <div class="dropdown">
-                                <button class="btn btn-sm dropdown-toggle" type="button" id="dr2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <span class="text-muted sr-only">Action</span>
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dr2">
-                                    <a class="dropdown-item" href="#">Edit</a>
-                                    <a class="dropdown-item" href="#">Remove</a>
-                                    <a class="dropdown-item" href="#">Assign</a>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                    <! --@endforeach -->
-                    <!-- delete -->
-                    <tr>
-                        <td>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="d3">
-                                <label class="custom-control-label" for="d3"></label>
-                            </div>
-                        </td>
-                        <td>2747</td>
-                        <td>Hester, Nissim L.</td>
-                        <td>4577 Cras St.</td>
-                        <td>(977) 220-6518</td>
-                        <td>Nov 4, 2019</td>
-                        <td>
-                            <div class="dropdown">
-                                <button class="btn btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <span class="text-muted sr-only">Action</span>
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-right">
-                                    <a class="dropdown-item" href="#">Edit</a>
-                                    <a class="dropdown-item" href="#">Remove</a>
-                                    <a class="dropdown-item" href="#">Assign</a>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="d4">
-                                <label class="custom-control-label" for="d4"></label>
-                            </div>
-                        </td>
-                        <td>2639</td>
-                        <td>Gardner, Leigh S.</td>
-                        <td>P.O. Box 228, 7512 Lectus Ave</td>
-                        <td>(537) 315-1481</td>
-                        <td>04/08/2019</td>
-                        <td>
-                            <div class="dropdown">
-                                <button class="btn btn-sm dropdown-toggle" type="button" id="dr4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <span class="text-muted sr-only">Action</span>
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dr4">
-                                    <a class="dropdown-item" href="#">Edit</a>
-                                    <a class="dropdown-item" href="#">Remove</a>
-                                    <a class="dropdown-item" href="#">Assign</a>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="d5">
-                                <label class="custom-control-label" for="d5"></label>
-                            </div>
-                        </td>
-                        <td>2238</td>
-                        <td>Higgins, Uriah L.</td>
-                        <td>Ap #377-5357 Sed Road</td>
-                        <td>(238) 386-0247</td>
-                        <td>Jan 13, 2020</td>
-                        <td>
-                            <div class="dropdown">
-                                <button class="btn btn-sm dropdown-toggle" type="button" id="dr5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <span class="text-muted sr-only">Action</span>
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dr5">
-                                    <a class="dropdown-item" href="#">Edit</a>
-                                    <a class="dropdown-item" href="#">Remove</a>
-                                    <a class="dropdown-item" href="#">Assign</a>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                    <!-- end delete -->
+                    <!-- endforeach -->
                     </tbody>
                 </table>
             </div>

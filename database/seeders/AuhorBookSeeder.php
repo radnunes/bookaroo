96a5956cdf7eeb36246a3d499d895f879bcd00d0
João Vicente <joao.vicente.t0126979@edu.atec.pt>
2024-10-08 22:05:23 +0100

    AuthorBookSeeder

--- database/seeders/AuhorBookSeeder.php
+++ database/seeders/AuhorBookSeeder.php
@@ -16,7 +16,8 @@ class AuhorBookSeeder extends Seeder
         DB::table('author_book')->insert([
             ['author_id'=>1,'book_id'=>1],
             ['author_id'=>1,'book_id'=>2],
-            ['author_id'=>1,'book_id'=>3]
+            ['author_id'=>2,'book_id'=>3],
+            ['author_id'=>3,'book_id'=>3],
         ]);
     }
 }

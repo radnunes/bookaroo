14d41a7484aa2e37cad1f1c2040840e091147ad5
João Vicente <joao.vicente.t0126979@edu.atec.pt>
2024-10-10 21:34:33 +0100

    web.php fixed

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\AuthorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BookController::class, 'index'])->name('home');

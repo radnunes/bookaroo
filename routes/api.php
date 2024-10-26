e44346a8bc7716b0959305eedcbc4fb428deaffc
sleeper <sleepervoid.c@gmail.com>
2024-10-26 16:57:34 +0100

    Api book index and show routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\BookController;
 use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,6 +9,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+/*
 Route::get('books', function () {
     return response()->json(Book::all());
 });
+*/
+
+Route::name('api.')->group(function () {
+    Route::resource('books', BookController::class)
+        ->parameters(['books' => 'book'])->only(['index', 'show']);
+});
+
